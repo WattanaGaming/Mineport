21c69ef148b59f10a4e045a4c8c75d1785dde2b5
Atirut Wattanamongkol <artjang301@gmail.com>
2021-07-08 17:18:05 +0700

    Update init.lua

--- mods/mineport/init.lua
+++ mods/mineport/init.lua
@@ -4,8 +4,8 @@ mineport = {
 
 minetest.log("action", "Running scripts")
 mtworks.dirTree(mineport.path.."/scripts", function(path)
-    minetest.log("action", "    "..path)
     if path:match("^.+(%..+)$") == ".lua" then
+        minetest.log("action", "> "..path)
         dofile(path)
     end
 end)
